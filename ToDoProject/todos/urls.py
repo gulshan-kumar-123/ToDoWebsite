832be12deeaedfe5819e80a3710baee313b9cc61
ABC <gulshanpandit123@gmail.com>
2023-06-02 23:38:51 +0530

    Added delete functionality

--- ToDoProject/todos/urls.py
+++ ToDoProject/todos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[
     path('list/',views.list_todo_items),
     path('insert_todo/',views.insert_todo_item,name='insert_todo_item'),
+    path('delete_todo/<int:todo_id>/',views.delete_todo_item,name='delete_todo_item'),
 ]
